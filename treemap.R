9ed805a172e0afbcfd4d86d42040b2b6c2cadeec
Eric Son <ym.eric.son@gmail.com>
2020-10-26 16:21:40 -0500

    grouped smaller job titles into "Other"

--- treemap.R
+++ treemap.R
@@ -19,41 +19,47 @@ df$department <- str_to_title(df$department)
 df$job_titles <- str_to_title(df$job_titles)
 df <- df %>% select("name", "department", "job_titles", "annual_salary")
 
-# change smaller departments to "Other"
-top_level1s <- head(names(sort(table(df$department), decreasing = TRUE)), 15)
+# change smaller departments to "OTHER DEPTS"
 df$level1 <- df$department
-df$level1[!(df$level1 %in% top_level1s)] <- "OTHER"
 df$level2 <- df$job_titles
-df[which(df$level1 == "OTHER"),]$level2 <- df[which(df$level1 == "OTHER"),]$department
 df$level3 <- NA
-df[which(df$level1 == "OTHER"),]$level3 <- df[which(df$level1 == "OTHER"),]$job_titles
+top_depts <- head(names(sort(table(df$department), decreasing = TRUE)), 15)
+df$level1[!(df$level1 %in% top_depts)] <- "OTHER DEPTS"
+other_dept_rows <- (df$level1 == "OTHER DEPTS")
+df[other_dept_rows,]$level2 <- df[other_dept_rows,]$department
+df[other_dept_rows,]$level3 <- df[other_dept_rows,]$job_titles
+
+# change smaller job titles to "Other"
+for (dept in top_depts) {
+  top_jobs <- head(names(sort(table(df[which(df$department == dept),]$job_titles), decreasing = TRUE)), 15)
+  df[(df$department == dept) & !(df$job_titles %in% top_jobs),]$level2 <- "OTHER"
+  dept_and_other_job <- (df$level1 == dept) & (df$level2 == "OTHER")
+  df[dept_and_other_job,]$level3 <- df[dept_and_other_job,]$job_titles
+}
 
 
-head(sort(table(df[which(df$department == "Police"),]$job_titles), decreasing = TRUE), 5)
-df$level1[!(df$level1 %in% top_level1s)] <- "OTHER"
-# head(subset(df, level1 == "OTHER"), 10)
-
-# group by level1 and job, convert numeric to USD
-level1_tot <- aggregate(df$annual_salary, by=list(Category=df$level1), FUN=sum) %>%
-  rename(level1 = Category, tot_salary_level1 = x)
-level1_tot$tot_salary_level1 <- paste('$', formatC(level1_tot$tot_salary_level1, big.mark=',', format = 'f'))
-level1_tot$tot_salary_level1 <- substr(level1_tot$tot_salary_level1, 1, nchar(level1_tot$tot_salary_level1)-5)
-
-job_tot <- aggregate(df$annual_salary, by=list(Category=df$level2), FUN=sum) %>%
-  rename(level2 = Category, tot_salary_job = x)
-job_tot$tot_salary_job <- paste('$', formatC(job_tot$tot_salary_job, big.mark=',', format = 'f'))
-job_tot$tot_salary_job <- substr(job_tot$tot_salary_job, 1, nchar(job_tot$tot_salary_job)-5)
-
-# add salary to level1 and job title
-df <- merge(merge(df, level1_tot, by="level1"), job_tot, by="level2")
-df$tot_salary_level1 <- do.call("paste", c(df[c("level1", "tot_salary_level1")], sep = "\n"))
-df$tot_salary_job <- do.call("paste", c(df[c("level2", "tot_salary_job")], sep = "\n"))
+# # group by level1 and job, convert numeric to USD
+# level1_tot <- aggregate(df$annual_salary, by=list(Category=df$level1), FUN=sum) %>%
+#   rename(level1 = Category, tot_salary_level1 = x)
+# level1_tot$tot_salary_level1 <- paste('$', formatC(level1_tot$tot_salary_level1, big.mark=',', format = 'f'))
+# level1_tot$tot_salary_level1 <- substr(level1_tot$tot_salary_level1, 1, nchar(level1_tot$tot_salary_level1)-5)
+# 
+# job_tot <- aggregate(df$annual_salary, by=list(Category=df$level2), FUN=sum) %>%
+#   rename(level2 = Category, tot_salary_job = x)
+# job_tot$tot_salary_job <- paste('$', formatC(job_tot$tot_salary_job, big.mark=',', format = 'f'))
+# job_tot$tot_salary_job <- substr(job_tot$tot_salary_job, 1, nchar(job_tot$tot_salary_job)-5)
+# 
+# 
+# # add salary to level1 and job title
+# df <- merge(merge(df, level1_tot, by="level1"), job_tot, by="level2")
+# df$tot_salary_level1 <- do.call("paste", c(df[c("level1", "tot_salary_level1")], sep = "\n"))
+# df$tot_salary_job <- do.call("paste", c(df[c("level2", "tot_salary_job")], sep = "\n"))
 
 
 
 # basic treemap
 chicago_treemap <- treemap(df,
-                           index=c("tot_salary_level1","tot_salary_job", "level3"),
+                           index=c("level1", "level2", "level3"),
                            vSize="annual_salary",
                            vColor="level1",
                            type="index",
@@ -74,7 +80,6 @@ chicago_inter <- d3tree2(chicago_treemap,  rootname="Salaries")
 
 
 
-head(names(sort(table(df$department), decreasing = TRUE)), 15)
 
 
 # save the widget
