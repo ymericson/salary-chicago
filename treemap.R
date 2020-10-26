c1cd90c3735aeff07457590ad4dbaefc0ee83233
Eric Son <ym.eric.son@gmail.com>
2020-10-26 14:59:22 -0500

    grouped smaller departments into "Other"

--- treemap.R
+++ treemap.R
@@ -13,38 +13,53 @@ library(htmltools)
 df <- read.socrata("https://data.cityofchicago.org/resource/n4bx-5kf6.json")
 cols.num <- c("frequency_description", "annual_salary","hourly_rate")
 df[cols.num] <- sapply(df[cols.num],as.numeric)
-df$annual_salary <- ifelse(is.na(df$annual_salary), df$frequency_description * df$hourly_rate * 50, df$annual_salary)   
-df$department <- str_to_title(c(df$department))
-df$job_titles <- str_to_title(c(df$job_titles))
+df$annual_salary <- ifelse(is.na(df$annual_salary), df$frequency_description * df$hourly_rate * 50, df$annual_salary)
+df$name <- str_to_title(df$name)
+df$department <- str_to_title(df$department)
+df$job_titles <- str_to_title(df$job_titles)
 df <- df %>% select("name", "department", "job_titles", "annual_salary")
 
-# group by dept and job, convert numeric to USD
-dept_tot <- aggregate(df$annual_salary, by=list(Category=df$department), FUN=sum) %>%
-  rename(department = Category, tot_salary_dept = x)
-dept_tot$tot_salary_dept <- paste('$', formatC(dept_tot$tot_salary_dept, big.mark=',', format = 'f'))
-dept_tot$tot_salary_dept <- substr(dept_tot$tot_salary_dept, 1, nchar(dept_tot$tot_salary_dept)-5)
+# change smaller departments to "Other"
+top_level1s <- head(names(sort(table(df$department), decreasing = TRUE)), 15)
+df$level1 <- df$department
+df$level1[!(df$level1 %in% top_level1s)] <- "OTHER"
+df$level2 <- df$job_titles
+df[which(df$level1 == "OTHER"),]$level2 <- df[which(df$level1 == "OTHER"),]$department
+df$level3 <- NA
+df[which(df$level1 == "OTHER"),]$level3 <- df[which(df$level1 == "OTHER"),]$job_titles
 
-job_tot <- aggregate(df$annual_salary, by=list(Category=df$job_titles), FUN=sum) %>%
-  rename(job_titles = Category, tot_salary_job = x)
+
+head(sort(table(df[which(df$department == "Police"),]$job_titles), decreasing = TRUE), 5)
+df$level1[!(df$level1 %in% top_level1s)] <- "OTHER"
+# head(subset(df, level1 == "OTHER"), 10)
+
+# group by level1 and job, convert numeric to USD
+level1_tot <- aggregate(df$annual_salary, by=list(Category=df$level1), FUN=sum) %>%
+  rename(level1 = Category, tot_salary_level1 = x)
+level1_tot$tot_salary_level1 <- paste('$', formatC(level1_tot$tot_salary_level1, big.mark=',', format = 'f'))
+level1_tot$tot_salary_level1 <- substr(level1_tot$tot_salary_level1, 1, nchar(level1_tot$tot_salary_level1)-5)
+
+job_tot <- aggregate(df$annual_salary, by=list(Category=df$level2), FUN=sum) %>%
+  rename(level2 = Category, tot_salary_job = x)
 job_tot$tot_salary_job <- paste('$', formatC(job_tot$tot_salary_job, big.mark=',', format = 'f'))
 job_tot$tot_salary_job <- substr(job_tot$tot_salary_job, 1, nchar(job_tot$tot_salary_job)-5)
 
-# add salary to department and job title
-df <- merge(merge(df, dept_tot, by="department"), job_tot, by="job_titles")
-df$tot_salary_dept <- do.call("paste", c(df[c("department", "tot_salary_dept")], sep = "\n"))
-df$tot_salary_job <- do.call("paste", c(df[c("job_titles", "tot_salary_job")], sep = "\n"))
+# add salary to level1 and job title
+df <- merge(merge(df, level1_tot, by="level1"), job_tot, by="level2")
+df$tot_salary_level1 <- do.call("paste", c(df[c("level1", "tot_salary_level1")], sep = "\n"))
+df$tot_salary_job <- do.call("paste", c(df[c("level2", "tot_salary_job")], sep = "\n"))
 
 
 
 # basic treemap
 chicago_treemap <- treemap(df,
-                           index=c("tot_salary_dept","tot_salary_job"),
+                           index=c("tot_salary_level1","tot_salary_job", "level3"),
                            vSize="annual_salary",
-                           vColor="department",
+                           vColor="level1",
                            type="index",
                            title="Chicago Salaries by Department and Job Title",
                            n=1,
-                           palette="Set2",
+                           palette="Set3",
                            bg.labels=c("white"),
                            align.labels=list(
                              c("center", "center"), 
@@ -56,6 +71,10 @@ chicago_treemap <- treemap(df,
 chicago_inter <- d3tree2(chicago_treemap,  rootname="Salaries")
 
 
+
+
+
+head(names(sort(table(df$department), decreasing = TRUE)), 15)
 
 
 # save the widget
