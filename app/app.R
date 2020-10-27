e444aed7535d71554d84a0fbe1153e984b448887
Eric Son <ym.eric.son@gmail.com>
2020-10-27 18:05:15 -0500

    added datatable in mainPanel

--- app/app.R
+++ app/app.R
@@ -8,45 +8,87 @@
 #
 
 library(shiny)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    tags$head(tags$style(
+        HTML('
+        #sidebar {
+            background-color: #e6f0ff;
+            }
+        body, label, input, button, select { 
+            font-family: "Calibri";
+            font-size: 13px;
+            }
+        #mytable tr:hover {
+             background-color: #e6f0ff
+             }
+        
+             
+             ')
+    )),
     titlePanel("App"),
     
     sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(id="sidebar",
             helpText("Filter employees with the following:"),
-            
             textInput("lname",
                       label = "Name", 
                       placeholder = "Last Name"),
-            selectInput("department",
+            selectInput("dept",
                         label = "Department",
                         selected = NULL,
                         choices = sort(unique(df_app$Department), decreasing = FALSE),
                         multiple = TRUE),
-            sliderInput("salary", 
+            sliderInput("range", 
                         label = "Salary Range",
-                        min = 0, max = max(df_app$`Annual Salary`), value = c(0, 0))
-        ),
-         
-        mainPanel()
+                        min = 0, max = max(df_app$`Salary`), value = c(0, 0)),
+            actionButton("search", "Search"),
+            actionButton("clear", "Clear Results")
+            ),
+        mainPanel(
+            dataTableOutput("mytable"),
+            textOutput("selected_var"),
+            textOutput("min_max")
+        )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    output$selected_var <- renderText({ 
-        paste("You have selected", input$var)
-    })
+    output$mytable <- renderDataTable(
+        datatable(df_app, 
+                  options = list(
+                   
+                      pageLength = 30, lengthChange = FALSE, searching = FALSE),
+                  rownames = FALSE,
+                  selection = 'none'
+                  ) %>% 
+            formatCurrency("Salary")
+        )
     
-    output$min_max <- renderText({ 
-        paste("You have chosen a range that goes from",
-              input$range[1], "to", input$range[2])
-    })
+    # 
+    # output$mytable = renderDataTable(
+    #     datatable(df_app %>% formatCurrency("Salary"),
+    #     options = list(pageLength = 30, lengthChange = FALSE),
+    #     rownames = FALSE)
+    # )
+}    
     
-}
+#     # reactive expression
+#     text_reactive <- eventReactive( input$search, {
+#         input$dept
+#     })
+#     
+#     # text output
+#     output$selected_var <- renderText({
+#         text_reactive()
+#     })
+# }
+
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
