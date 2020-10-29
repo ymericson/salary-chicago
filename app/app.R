5ffad912c74c772a210a0d22175d77f7de02f7d4
Eric Son <ym.eric.son@gmail.com>
2020-10-29 00:54:31 -0500

    added job title input field

--- app/app.R
+++ app/app.R
@@ -10,7 +10,7 @@ ui <- fluidPage(
             }
         body, label, input, button, select { 
             font-family: "Calibri";
-            font-size: 13px;
+            font-size: 12px;
             }
         #mytable tr:hover {
             background-color: #e6f0ff
@@ -27,8 +27,14 @@ ui <- fluidPage(
             selectInput("dept",
                         label = "Department",
                         selected = NULL,
-                        choices = sort(unique(df_app$Department), decreasing = FALSE),
+                        choices = sort(unique(df_app$deptFreq), decreasing = FALSE),
                         multiple = TRUE),
+            selectInput("job",
+                        label = "Job Title",
+                        selected = NULL,
+                        choices = sort(unique(df_app$Job), decreasing = FALSE),
+                        multiple = TRUE),
+            uiOutput("secondSelection"),
             sliderInput("range", 
                         label = "Salary Range",
                         min = 0, max = max(df_app$`Salary`), 
@@ -36,8 +42,6 @@ ui <- fluidPage(
             actionButton("clear", "Clear")
             ),
         mainPanel(
-            textOutput("name"),
-            textOutput("range"),
             dataTableOutput("data")
         )
     )
@@ -56,10 +60,11 @@ filter_var <- function(x, val) {
 server <- function(input, output, session) {
     selected <- reactive({
         str_detect(str_split_fixed(df_app$Name, ",", 2)[,1], str_to_title(input$name)) &
-        filter_var(df_app$Department, input$dept) &
+        filter_var(df_app$deptFreq, input$dept) &
+        filter_var(df_app$Job, input$job) &    
         filter_var(df_app$Salary, input$range)
     })
-    output$data <- renderDataTable(datatable(df_app[selected(),],
+    output$data <- renderDataTable(datatable(df_app[selected(),] %>% select("Name", "Department", "Job", "Salary"),
                                              options = list(pageLength = 25,
                                                             lengthChange = FALSE,
                                                             searching = FALSE),
@@ -68,7 +73,7 @@ server <- function(input, output, session) {
                                              ) %>%
                                        formatCurrency("Salary") 
     )
-    output$name <- renderText({ input$name })
+
 }
 
 
